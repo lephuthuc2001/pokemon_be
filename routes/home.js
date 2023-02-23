03aa671841ce2e6a692327a646bf0092fe2f032b
lephuthuc2001 <lephuthuc2001@gmail.com>
2023-02-23 20:13:34 +0700

    adding details page

--- routes/home.js
+++ routes/home.js
@@ -2,8 +2,9 @@ const csv = require("csvtojson");
 const rootDir = require("./root");
 const path = require("path");
 const fs = require("fs");
-const { hostname } = require("os");
 const router = require("express").Router();
+const dbPath = path.join(rootDir, "pokemons.json");
+const { faker } = require("@faker-js/faker");
 
 const pokemonTypes = [
   "bug",
@@ -46,13 +47,16 @@ const init = async () => {
         url: `https://raw.githubusercontent.com/lephuthuc2001/pokemon_be/main/public/pokemon_images/${
           index + 1
         }.jpg`,
+        description: faker.lorem.paragraph(3),
+        height: faker.address.zipCode("###") + "cm",
+        weight: faker.address.zipCode("###") + "kg",
+        category: faker.word.adjective(),
+        abilities: faker.music.songName(),
       };
     }
   });
 
   pokemonsData = pokemonsData.filter((pokemon) => !!pokemon);
-
-  const dbPath = path.join(rootDir, "pokemons.json");
 
   fs.writeFileSync(
     dbPath,
@@ -106,6 +110,57 @@ router.get("/:pokemonId", async function (req, res, next) {
   res.send({ data: result });
 });
 
+router.post("/", function (req, res, next) {
+  let data = getDataFromDb();
+
+  data = JSON.parse(data);
+
+  const { name, id, url, types } = req.body;
+
+  ////"Missing required data
+  if (!name || !id || !url || !types) {
+    res.status(401).send({ message: "Missing required value" });
+  }
+  ///Pokémon can only have one or two types
+  if (types[0]) {
+    if (!pokemonTypes.includes(types[0])) {
+      res.status(401).send({ message: "Pokémon's type is invalid" });
+    }
+  }
+
+  if (types[1]) {
+    if (!pokemonTypes.includes(types[1])) {
+      res.status(401).send({ message: "Pokémon's type is invalid" });
+    }
+  }
+
+  if (
+    data.data.find(
+      (pokemon) => pokemon.name.toLowerCase() === name.toLowerCase()
+    ) ||
+    data.data.find((pokemon) => parseInt(pokemon.id) === parseInt(id))
+  ) {
+    res.status(401).send({ message: "Pokémon is existing" });
+  }
+
+  data.data.push({
+    id: parseInt(id),
+    name,
+    types: types.filter((type) => type !== null),
+    url,
+  });
+
+  fs.writeFileSync(
+    dbPath,
+    JSON.stringify({
+      data: data.data,
+      totalPokemons: data.data.length,
+    })
+  );
+
+  res.send({ message: "success" });
+});
+
 router.get("/", async function (req, res, next) {
   let pokemonsData = getDataFromDb();
 
@@ -125,7 +180,9 @@ router.get("/", async function (req, res, next) {
   }
   ///search
   if (search) {
-    result = result.filter((pokemon) => pokemon.name.toLowerCase() === search);
+    result = result.filter(
+      (pokemon) => pokemon.name.toLowerCase() === search.toLowerCase()
+    );
   }
   //page
   result = result.slice(limit * (page - 1), limit * page);
